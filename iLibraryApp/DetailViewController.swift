725385a8e0fe031971eb9211d2430adbaed3e5ac
Angelo Vittorio Zapponi <vittorio@MacBook-Pro-di-Vittorio.local>
2015-08-29 14:09:46 +0200

    Initial Commit

---
+++ iLibraryApp/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  iLibraryApp
+//
+//  Created by Angelo Vittorio Zapponi on 29/08/15.
+//  Copyright (c) 2015 Angelo Vittorio Zapponi. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail: AnyObject = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.valueForKey("timeStamp")!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iLibraryAppTests/iLibraryAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  iLibraryAppTests.swift
+//  iLibraryAppTests
+//
+//  Created by Angelo Vittorio Zapponi on 29/08/15.
+//  Copyright (c) 2015 Angelo Vittorio Zapponi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class iLibraryAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
